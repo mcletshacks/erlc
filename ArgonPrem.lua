fec3e5bd39b27bee8819450a0fe632a96417be5f
mcletshacks <77382229+mcletshacks@users.noreply.github.com>
2024-04-03 08:25:57 +0200

    Update ArgonPrem.lua

--- ArgonPrem.lua
+++ ArgonPrem.lua
@@ -1,2 +1,2 @@
---v5
-loadstring(game:HttpGet("https://raw.githubusercontent.com/mcletshacks/ArgonHubV2/main/LoadPremium/Loader.lua", true))()
+--v6
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mcletshacks/Argon-Hub-V6/main/Main.lua", true))()
